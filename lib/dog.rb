a7e9f70aa01b35961998cb163984e5202268af85
Christian Thomas <christian.thomas.2894@gmail.com>
2018-07-23 21:45:51 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -43,8 +43,8 @@ class Dog
     self
   end
 
-  def self.create(attributes_hash)
-    dog = Dog.new
+  def self.create(name:, breed:)
+    dog = Dog.new(name: name, breed: breed)
     dog.save
     dog
   end
